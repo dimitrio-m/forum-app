f3f146fd187128f6fa1a8530e00873bc0d0bf7c0
Dimitrio Mandamadiotis <dimitriom@idealcode.io>
2021-12-02 15:28:35 -0400

    Implemented not found page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-large text-center">
+    <h1 class="title">
+      Not Found
+    </h1>
+    <router-link :to="{ name: 'Home' }">
+      Read some cool forums
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,32 @@ import {createRouter, createWebHistory} from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import PageForumShow from '@/components/PageForumShow.vue'
+import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   { path: '/', name: 'Home', component: PageHome },
   { path: '/forum/:id', name: 'ForumShow', component: PageForumShow, props: true },
-  { path: '/thread/:id', name: 'ThreadShow', component: PageThreadShow, props: true },
-
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true,
+    beforeEnter(to, from, next) {
+      const threadExists = sourceData.threads.some(thread => thread.id === to.params.id)
+      if (threadExists) {
+        next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash,
+        })
+      }
+    }
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound },
 ]
 
 export default createRouter({
