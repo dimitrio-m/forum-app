56af53a848068e1594c2b32f3a334e16c57af2ac
Dimitrio Mandamadiotis <me@dimitriosmandamadiotis.com>
2021-12-06 10:14:13 -0400

    Added progress bar too

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,19 @@
   <div class="container">
     <router-view
       v-show="showPage"
-      @ready="showPage = true"
+      @ready="onPageReady"
     />
-    <app-spinner
-      v-show="!showPage"
-    >
-      loading...
-    </app-spinner>
+    <AppSpinner v-show="!showPage" />
   </div>
 </template>
 
 <script>
-import TheNavbar from '@/components/TheNavbar.vue'
+import TheNavbar from '@/components/TheNavbar'
 import { mapActions } from 'vuex'
-
+import NProgress from 'nprogress'
 export default {
   name: 'App',
-  components: {
-    TheNavbar
-  },
+  components: { TheNavbar },
   data () {
     return {
       showPage: false
@@ -29,16 +23,29 @@ export default {
   },
   created () {
     this.fetchAuthUser()
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
     this.$router.beforeEach(() => {
       this.showPage = false
+      NProgress.start()
     })
   },
   methods: {
-    ...mapActions(['fetchAuthUser'])
+    ...mapActions(['fetchAuthUser']),
+    onPageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
   }
 }
 </script>
 
 <style>
 @import "assets/style.css";
+@import "~nprogress/nprogress.css";
+#nprogress .bar{
+  background: #57AD8D !important;
+}
 </style>
